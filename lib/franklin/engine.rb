e2db93bea36edd8cbdd9a076fbe39daf41024b0f
Gk <gk@gkparishphilp.com>
2019-02-04 10:07:30 -0800

    - some migr

--- lib/franklin/engine.rb
+++ lib/franklin/engine.rb
@@ -1,5 +1,30 @@
+
+# required gems from gemspec
+require 'acts-as-taggable-array-on'
+
+
 module Franklin
-  class Engine < ::Rails::Engine
-    isolate_namespace Franklin
-  end
+
+	class << self
+
+		# engine configuration settings accessors
+		mattr_accessor :courses_path
+
+		# settings defaults
+		self.courses_path = '/courses'
+
+	end
+
+	# this function maps the vars from your app into the engine
+	def self.configure( &block )
+		yield self
+	end
+
+
+	class Engine < ::Rails::Engine
+		isolate_namespace Franklin
+	end
 end
+
+
+
---
+++ lib/tasks/install_files/migrations/franklin_migration.rb
@@ -0,0 +1,86 @@
+class FranklinMigration < ActiveRecord::Migration[5.1]
+
+	def change
+
+		create_table :franklin_metrics do |t|
+			t.string			:title
+			t.integer  			:movement_id
+			t.string   			:title
+			t.string   			:slug
+			t.text     			:aliases,            default: [],         array: true
+			t.integer  			:user_id
+			t.string   			:default_value_type
+			t.text     			:description
+			t.integer  			:availability,       default: 0
+			t.integer  			:unit_id
+			t.string   			:default_period,     default: :all_time
+			t.datetime 			:created_at
+			t.datetime 			:updated_at
+			t.integer  			:category_id
+			
+			t.timestamps
+		end
+
+		create_table :franklin_observations do |t|
+			t.string   			:tmp_id
+			t.integer  			:user_id
+			t.integer  			:parent_id
+			t.integer  			:lft
+			t.integer  			:rgt
+			t.integer  			:observed_id
+			t.string   			:observed_type
+			t.string   			:title
+			t.string   			:content
+			t.float    			:value
+			t.string   			:rx
+			t.text     			:notes
+			t.datetime 			:started_at
+			t.datetime 			:ended_at
+			t.datetime 			:recorded_at
+			t.hstore   			:properties,    default: {}
+			t.datetime 			:created_at
+			t.datetime 			:updated_at
+			t.integer  			:unit_id
+			t.integer  			:status,        default: 1
+		end
+
+		create_table :franklin_targets do |t|
+			t.integer  			:parent_obj_id
+			t.string   			:parent_obj_type
+			t.integer  			:user_id
+			t.integer  			:unit_id
+			t.string   			:target_type,     default: :value
+			t.float    			:value
+			t.float    			:min
+			t.float    			:max
+			t.string   			:direction,       default: :at_most
+			t.string   			:period,          default: :all_time
+			t.datetime 			:start_at
+			t.datetime 			:end_at
+			t.integer  			:status,          default: 1
+			t.datetime 			:created_at
+			t.datetime 			:updated_at
+    	end
+
+    	create_table :franklin_units, force: :cascade do |t|
+			t.integer  			:base_unit_id
+			t.integer  			:imperial_correlate_id
+			t.integer  			:user_id
+			t.integer  			:metric_id
+			t.float    			:conversion_factor,        default: 1.0
+			t.string   			:name
+			t.string   			:slug
+			t.string   			:abbrev
+			t.integer  			:unit_type,                default: 0
+			t.text     			:aliases,                  default: [],   array: true
+			t.boolean  			:imperial,                 default: true
+			t.datetime 			:created_at
+			t.datetime 			:updated_at
+			t.integer  			:custom_base_unit_id
+			t.float    			:custom_conversion_factor
+		end
+
+		
+
+	end
+end
